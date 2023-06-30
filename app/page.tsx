d0d699d905215b7ed7605bac564a0499f05e4119
balldev1 <132042116+balldev1@users.noreply.github.com>
2023-06-30 12:27:32 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import getCurrentUser from './actions/getCurrentUser';
 import getListings, { IListingsParams } from './actions/getListings';
 
@@ -7,7 +9,6 @@ import Container from './components/Container'
 import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
 
-export const dynamic = 'force-dynamic'
 
 interface HomeProps {
   searchParams: IListingsParams
